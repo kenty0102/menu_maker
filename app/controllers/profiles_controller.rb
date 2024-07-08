8ce5771f417758652785bd787c48dcf54f967c2d
藤田健仁 <fujitakento@fujitakenjinoMacBook-Air.local>
2024-07-09 01:37:25 +0900

    ユーザー名変更機能を実装

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,14 +1,27 @@
 class ProfilesController < ApplicationController
   before_action :require_login
-  before_action :set_user, only: %i[show edit_username]
+  before_action :set_user, only: %i[show edit_username update_username]
 
   def show; end
 
   def edit_username; end
 
+  def update_username
+    if @user.update(username_params)
+      redirect_to profile_path, success: t('.success')
+    else
+      flash.now[:danger] = t('.failure')
+      render :edit_username, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_user
     @user = User.find(current_user.id)
+  end
+
+  def username_params
+    params.require(:user).permit(:name)
   end
 end

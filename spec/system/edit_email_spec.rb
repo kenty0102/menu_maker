0bf2563173b71061b5901e69f79ab0605ec9a6f7
藤田健仁 <fujitakento@fujitakenjinoMacBook-Air.local>
2024-07-10 13:29:46 +0900

    メールアドレス変更に関するテストコードを作成

---
+++ spec/system/edit_email_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe 'メールアドレスの変更', type: :system do
+  let(:user) { create(:user) }
+
+  before do
+    login(user)
+    click_link 'マイページ'
+    click_link 'edit_email_link'
+  end
+
+  context "when 正しい情報が入力された場合" do
+    it '正しい情報を入力した場合、変更に成功すること' do
+      fill_in 'new_email', with: 'update@example.com'
+      click_button '変更'
+      visit profile_path
+      expect(user.reload.email).to eq 'update@example.com'
+    end
+
+    it '変更に成功した時にフラッシュメッセージが表示されること' do
+      fill_in 'new_email', with: 'update@example.com'
+      click_button '変更'
+      expect(page).to have_content 'メールアドレスを変更しました'
+    end
+  end
+
+  context "when 不正な情報が入力された場合" do
+    it 'メールアドレスの形式が不正な場合、エラーメッセージが表示される' do
+      fill_in 'new_email', with: 'update.example.com'
+      click_button '変更'
+      expect(page).to have_content 'メールアドレスの変更に失敗しました'
+    end
+
+    it 'すでに使用されているアドレスを入力した場合、エラーメッセージが表示される' do
+      create(:user, email: 'existing@example.com')
+      fill_in 'new_email', with: 'existing@example.com'
+      click_button '変更'
+      expect(page).to have_content 'メールアドレスの変更に失敗しました'
+    end
+  end
+end
--- spec/system/edit_username_spec.rb
+++ spec/system/edit_username_spec.rb
@@ -9,22 +9,26 @@ RSpec.describe 'ユーザー名の変更', type: :system do
     click_link 'edit_username_link'
   end
 
-  it '正しい情報を入力した場合、変更に成功すること' do
-    fill_in 'new_name', with: 'NewUsername'
-    click_button '変更'
-    visit profile_path
-    expect(user.reload.name).to eq 'NewUsername'
+  context "when 正しい情報が入力された場合" do
+    it '正しい情報を入力した場合、変更に成功すること' do
+      fill_in 'new_name', with: 'NewUsername'
+      click_button '変更'
+      visit profile_path
+      expect(user.reload.name).to eq 'NewUsername'
+    end
+
+    it '変更に成功した時にフラッシュメッセージが表示されること' do
+      fill_in 'new_name', with: 'NewUsername'
+      click_button '変更'
+      expect(page).to have_content 'ユーザー名を変更しました'
+    end
   end
 
-  it '変更に成功した時にフラッシュメッセージが表示されること' do
-    fill_in 'new_name', with: 'NewUsername'
-    click_button '変更'
-    expect(page).to have_content 'ユーザー名を変更しました'
-  end
-
-  it 'ユーザー名が空の場合、エラーメッセージが表示される' do
-    fill_in 'new_name', with: ''
-    click_button '変更'
-    expect(page).to have_content 'ユーザー名の変更に失敗しました'
+  context "when 不正な情報が入力された場合" do
+    it 'ユーザー名が空の場合、エラーメッセージが表示される' do
+      fill_in 'new_name', with: ''
+      click_button '変更'
+      expect(page).to have_content 'ユーザー名の変更に失敗しました'
+    end
   end
 end

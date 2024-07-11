368d0aaaa5260ad1d3ec891fbe2a32f7e936a02a
藤田健仁 <fujitakento@fujitakenjinoMacBook-Air.local>
2024-07-11 16:20:05 +0900

    GitHub Actions関連のファイルを修正(1)

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,9 +6,3 @@ Capybara.register_driver :remote_chrome do |app|
   options.add_argument('window-size=1680,1050')
   Capybara::Selenium::Driver.new(app, browser: :remote, url: ENV.fetch('SELENIUM_DRIVER_URL'), capabilities: options)
 end
-
-Capybara.configure do |config|
-  config.server = :puma, { Silent: true }  # Rails 5+ デフォルトサーバー
-  config.server_host = "0.0.0.0"
-  config.server_port = ENV.fetch('CAPYBARA_SERVER_PORT', 3001)
-end

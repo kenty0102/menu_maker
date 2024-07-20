a3075201ada99973df4d3e840d37337dbe950e34
藤田健仁 <fujitakento@fujitakenjinoMacBook-Air.local>
2024-07-20 17:10:30 +0900

    レシピの編集機能を実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -76,6 +76,17 @@ class RecipesController < ApplicationController
     @recipe = current_user.recipes.find(params[:id])
   end
 
+  def update
+    @recipe = current_user.recipes.find(params[:id])
+    updated_params = process_instructions(recipe_params)
+    if @recipe.update(updated_params)
+      redirect_to recipe_path(@recipe), success: t('.update_success')
+    else
+      flash.now[:danger] = t('.update_failure')
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def create
     @recipe = current_user.recipes.new(recipe_params)
     if @recipe.save
@@ -101,6 +112,19 @@ class RecipesController < ApplicationController
 
   def recipe_params
     params.require(:recipe).permit(:title, :image, :image_cache, ingredients_attributes: [:id, :name, :quantity, :unit, :_destroy], instructions_attributes: [:id, :step_number, :description, :_destroy])
+  end
+
+  def process_instructions(params)
+    if params[:instructions_attributes].present?
+      current_step = 1
+      params[:instructions_attributes].each do |key, value|
+        if value[:_destroy] != '1'
+          value[:step_number] = current_step
+          current_step += 1
+        end
+      end
+    end
+    params
   end
 
   def update_ingredients_and_instructions(recipe, page)

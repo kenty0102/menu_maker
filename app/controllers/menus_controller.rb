aed70447df466930afba5aa4f2b2e638c3e5c465
藤田健仁 <fujitakento@fujitakenjinoMacBook-Air.local>
2024-07-24 17:05:07 +0900

    メニュー表作成機能を実装

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,8 +1,84 @@
 class MenusController < ApplicationController
   before_action :require_login
 
+  def index
+    @menus = current_user.menus
+  end
+
+  def show
+    @menu = Menu.find(params[:id])
+    design = Design.find_by(name: '居酒屋風メニュー')
+    @layout = JSON.parse(design.layout)
+    @recipes = @menu.recipes
+  end
+
   def new
     @menu = Menu.new
     @recipes = current_user.recipes
+  end
+
+  def edit
+    @menu = current_user.menus.find(params[:id])
+    @recipes = current_user.recipes
+    @selected_recipes = @menu.recipes.pluck(:id)
+  end
+
+  def create
+    @menu = current_user.menus.new(menu_params)
+  
+    if @menu.save
+      params[:recipe_ids].each do |recipe_id|
+        MenuRecipe.create(menu_id: @menu.id, recipe_id: recipe_id)
+      end
+
+      default_design_id = 1
+      MenuDesign.create(menu_id: @menu.id, design_id: default_design_id)
+
+      redirect_to menu_path(@menu), success: t('.success')
+    else
+      flash.now[:danger] = t('.failure')
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @menu = current_user.menus.find(params[:id])
+
+    if @menu.update(menu_params)
+      # 新しいレシピのIDを取得
+      new_recipe_ids = params[:recipe_ids].reject(&:blank?).map(&:to_i)
+
+      # 現在のレシピのIDを取得
+      current_recipe_ids = @menu.recipes.pluck(:id)
+
+      # 新しく追加するレシピIDと削除するレシピIDを計算
+      recipes_to_add = new_recipe_ids - current_recipe_ids
+      recipes_to_remove = current_recipe_ids - new_recipe_ids
+
+      # 新しいレシピを追加
+      recipes_to_add.each do |recipe_id|
+        @menu.menu_recipes.create!(recipe_id: recipe_id)
+      end
+
+      # 古いレシピを削除
+      @menu.menu_recipes.where(recipe_id: recipes_to_remove).destroy_all
+
+      redirect_to menu_path(@menu), success: t('.success')
+    else
+      flash.now[:danger] = t('.failure')
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    menu = current_user.menus.find(params[:id])
+    menu.destroy!
+    redirect_to menus_path, success: t('.success')
+  end
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:title, recipe_ids: [])
   end
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,6 +2,8 @@ class Menu < ApplicationRecord
   belongs_to :user
   has_many :menu_recipes, dependent: :destroy
   has_many :recipes, through: :menu_recipes
+  has_many :menu_designs, dependent: :destroy
+  has_many :designs, through: :menu_designs
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
 end
